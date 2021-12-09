f153353d0f3190103339130bdc1f6f108bf8c65d
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2021-12-09 21:12:08 +0800

    add tag builder

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "hexlet_code/version"
+require_relative "hexlet_code/tag"
 
 module HexletCode
   class Error < StandardError; end
+
   # Your code goes here...
 end
---
+++ lib/hexlet_code/tag.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module HexletCode
+  class Tag
+    SINGLE_TAGS = %w[br input img].freeze
+
+    def self.build(tag, attrs = {})
+      html_attrs = attrs.map { |(key, value)| " #{key}='#{value}'" }.join
+      if SINGLE_TAGS.include?(tag)
+        "<#{tag}#{html_attrs}>"
+      else
+        "<#{tag}#{html_attrs}>#{block_given? ? yield : ""}</#{tag}>"
+      end
+    end
+  end
+end
